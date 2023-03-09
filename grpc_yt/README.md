e3629befa7cdf2da7d3e2a5741e9410c1c638144
volo h <vhavrylyak.it@gmail.com>
2023-03-09 17:58:45 +0200

    after lesson2

--- grpc_yt/README.md
+++ grpc_yt/README.md
@@ -11,6 +11,26 @@ inspired
   exhausting
 
 2 - https://www.youtube.com/watch?v=8SnC354B5Vc
+  https://github.com/grpc-ecosystem/grpc-gateway
+
+  run once:
+  ```sh
+  go install \
+      github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway \
+      github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2 \
+      google.golang.org/protobuf/cmd/protoc-gen-go \
+      google.golang.org/grpc/cmd/protoc-gen-go-grpc
+  ```
+  mkdir -p google/api
+  curl https://raw.githubusercontent.com/googleapis/googleapis/master/google/api/annotations.proto > google/api/annotations.proto
+  curl https://raw.githubusercontent.com/googleapis/googleapis/master/google/api/http.proto > google/api/http.proto
+
+  complains
+  simultaneously
+
 3 - https://www.youtube.com/watch?v=CTf3RonwC3E
+
+
+
 4 - https://www.youtube.com/watch?v=4NdS0hfhkyc
 
--- grpc_yt/server/server.go
+++ grpc_yt/server/server.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 	pb "grpc_yt/gen/proto"
 	"log"
 	"net"
+	"net/http"
 )
 
 type testApiServer struct {
@@ -21,12 +23,19 @@ func (s *testApiServer) Echo(ctx context.Context, req *pb.ResponseRequest) (*pb.
 }
 
 func main() {
+	go func() {
+		mux := runtime.NewServeMux()
+		pb.RegisterTestApiHandlerServer(context.Background(), mux, &testApiServer{})
+		log.Fatalln(http.ListenAndServe("localhost:8081", mux))
+	}()
+
 	listener, err := net.Listen("tcp", "localhost:8080")
 	if err != nil {
 		log.Fatalln(err)
 	}
-  grpcServer := grpc.NewServer()
-  pb.RegisterTestApiServer(grpcServer, &testApiServer{})
+
+	grpcServer := grpc.NewServer()
+  	pb.RegisterTestApiServer(grpcServer, &testApiServer{})
 
 	err = grpcServer.Serve(listener)
 
